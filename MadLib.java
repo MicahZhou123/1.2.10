2bc0ad2e7a2004c9fb575d19ef1e8b1085c7abd4
MicahZhou123 <30tbit@gmail.com>
2025-10-06 14:25:37 -0700

    Update MadLib.java

--- MadLib.java
+++ MadLib.java
@@ -4,24 +4,28 @@ public class MadLib {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter name: ");
-        String name = input.nextLine();
+        String madlib = "phone was feeling very banana one day so they grabbed their favorite orange 
+            and started berry. Eventually, they ended up in grape!";
 
-        System.out.print("Enter an adjective: ");
+        System.out.println(madlib);
+
+        System.out.print("Enter name:           (phone)");
+        String name = input.nextLine();
+        String replacement1 = name.replace("phone", name);
+
+        System.out.print("Enter an adjective:    (banana)");
         String adjective = input.nextLine();
 
-        System.out.print("Enter a noun: ");
+        System.out.print("Enter a noun:          (orange)");
         String noun = input.nextLine();
 
-        System.out.print("Enter a verb ending in -ing: ");
+        System.out.print("Enter a verb ending in -ing:       (berry)");
         String verb = input.nextLine();
 
-        System.out.print("Enter a place: ");
+        System.out.print("Enter a place:         (grape)");
         String place = input.nextLine();
 
-        System.out.println(name + " was feeling very " + adjective + " one day.");
-        System.out.println("So they grabbed their favorite " + noun + " and started " + verb + ".");
-        System.out.println("Eventually, they ended up in " + place + "!");
-
+        
     }
+
 }
