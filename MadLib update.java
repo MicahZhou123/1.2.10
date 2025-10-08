aa42c99e1af80a873c91d46da2540f47fe7de72d
MicahZhou123 <30tbit@gmail.com>
2025-10-08 15:20:29 -0700

    Update MadLib update.java

--- MadLib update.java
+++ MadLib update.java
@@ -4,8 +4,7 @@ public class MadLib {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String madlib = "(strawberry) was feeling very (banana) one day so they grabbed their favorite (orange) " +
-                        "and started (berry). Eventually, they ended up in grape!";
+        String madlib = "strawberry was feeling very banana one day so they grabbed their favorite orange and started berry. Eventually, they ended up in grape!";
 
         System.out.print("Enter name:           (strawberry): ");
         String name = input.nextLine();
@@ -22,16 +21,21 @@ public class MadLib {
         System.out.print("Enter a place (say a first though):         (grape): ");
         String place = input.nextLine();
 
-        int phoneIndex = madlib.indexOf("phone");
+        int strawberryIndex = madlib.indexOf("strawberry");
         int bananaIndex = madlib.indexOf("banana");
         int orangeIndex = madlib.indexOf("orange");
         int berryIndex = madlib.indexOf("berry");
         int grapeIndex = madlib.indexOf("grape");
 
-        String part1 = madlib.substring(0, phoneIndex);
-        String part2 = madlib.substring(phoneIndex + "phone".length(), bananaIndex);
+        String part1 = madlib.substring(0, strawberryIndex);
+        String part2 = madlib.substring(strawberryIndex + "strawberry".length(), bananaIndex);
+        String part3 = madlib.substring(bananaIndex + "banana".length(), orangeIndex);
+        //(debugging this) String part4 = madlib.substring(orangeIndex + "orange".length(), berryIndex);
+        //String part5 = madlib.substring(berryIndex + "berry".length(), grapeIndex);
 
-        String finalMadLib = part1 + name + part2;
+
+
+        String finalMadLib = part1 + name + part2 + adjective + part3 + noun;
         System.out.println(finalMadLib);
 
 
